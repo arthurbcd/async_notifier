35aefa530b144ba392bd99168c515b50c5cd9a5d
Arthur Miranda <60884977+arthurbcd@users.noreply.github.com>
2023-10-29 07:47:59 -0300

    Update async_snapshot_extension.dart

--- lib/src/extensions/async_snapshot_extension.dart
+++ lib/src/extensions/async_snapshot_extension.dart
@@ -3,13 +3,12 @@ import 'package:flutter/widgets.dart';
 /// Extension on [AsyncSnapshot] to handle its various states
 extension AsyncSnapshotExtension<T> on AsyncSnapshot<T> {
   /// Allows you to define custom behavior for different states of an [AsyncSnapshot]
-  /// by providing callbacks for `data`, `error`, `loading`, `reloading`, and `idle`.
+  /// by providing callbacks for `data`, `error`, `loading`, and `none`.
   ///
   /// - [data] Callback invoked when the snapshot contains data of type [T].
   /// - [error] Callback invoked when the snapshot contains an error.
   /// - [loading] Callback invoked when the snapshot is waiting for data.
-  /// - [reloading] Optional callback invoked when the snapshot is active and contains data.
-  /// - [none] Optional callback invoked when the snapshot !hasData and !hasError.
+  /// - [none] Optional callback invoked when the snapshot has no data or error.
   ///
   /// Example:
   /// ```dart
@@ -18,40 +17,32 @@ extension AsyncSnapshotExtension<T> on AsyncSnapshot<T> {
   ///   data: (data) => Text('Data: $data'),
   ///   error: (error, stack) => Text('Error: $error'),
   ///   loading: () => CircularProgressIndicator(),
-  ///   reloading: (data) => Text('Reloading: $data'),
   /// );
   /// ```
-  /// The extension performs type checks and assertions to ensure data consistency.
-  ///
-  /// - `ConnectionState.active` and `ConnectionState.done` expects data of type [T].
-  /// As [AsyncSnapshot] documentation states, it expects either data or error.
   R when<R>({
     required R Function(T data) data,
     required R Function(Object error, StackTrace stackTrace) error,
     required R Function() loading,
-    R Function(T data)? reloading,
     R Function()? none,
   }) {
-    if (hasError) return error(this.error!, stackTrace ?? StackTrace.current);
+    if (hasError) return error(this.error!, stackTrace ?? StackTrace.empty);
+    if (this.data is T) return data(this.data as T);
 
     switch (connectionState) {
       case ConnectionState.none:
-        if (this.data is! T) return none?.call() ?? loading();
-        return data(this.data as T);
+        return none?.call() ?? loading();
 
       case ConnectionState.waiting:
-        if (this.data is! T) return loading();
-        return reloading?.call(this.data as T) ?? loading();
+        return loading();
 
       case ConnectionState.active:
-        if (this.data is! T) return loading();
-        return reloading?.call(this.data as T) ?? data(this.data as T);
+        return loading();
 
       case ConnectionState.done:
-        if (this.data is! T) return none?.call() ?? data(requireData);
-        return data(this.data as T);
+        return none?.call() ?? data(requireData);
     }
   }
 
-
+  /// Returns whether this snapshot has neither data nor error.
+  bool get hasNone => !hasData && !hasError;
 }
