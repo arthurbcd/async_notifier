088e58212366909a3f85fb3ff74001b39b76edce
Arthur Miranda <60884977+arthurbcd@users.noreply.github.com>
2023-10-29 07:48:22 -0300

    Update async_listenable_extension.dart

--- lib/src/extensions/async_listenable_extension.dart
+++ lib/src/extensions/async_listenable_extension.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 
 import '../../async_notifier.dart';
@@ -8,18 +7,18 @@ import 'async_snapshot_extension.dart';
 /// Extension for [AsyncListenableBase].
 extension AsyncListenableBaseExtension<T, Data extends T>
     on AsyncListenableBase<T, Data> {
-  /// Wheter [future] or [stream] is computing for the first time.
+  /// Whether [future] or [stream] is computing for the first time.
   bool get isLoading => snapshot.connectionState == ConnectionState.waiting;
 
-  /// Wheter [future] or [stream] is recomputing.
+  /// Whether [future] or [stream] is recomputing.
   bool get isReloading =>
       (hasData || hasError) &&
       (isLoading || snapshot.connectionState == ConnectionState.active);
 
-  /// Wheter [future] or [stream] threw an error.
+  /// Whether [future] or [stream] threw an error.
   bool get hasError => snapshot.hasError;
 
-  /// Wheter [future] or [stream] has data.
+  /// Whether [future] or [stream] has data.
   bool get hasData => snapshot.hasData;
 
   /// The current error of [future] or [stream].
@@ -31,6 +30,9 @@ extension AsyncListenableBaseExtension<T, Data extends T>
   /// The current [ConnectionState] of [future] or [stream].
   ConnectionState get connectionState => snapshot.connectionState;
 
+  /// Whether this snapshot has neither data nor error.
+  bool get hasNone => snapshot.hasNone;
+
   /// Handles different states of this [snapshot].
   ///
   /// For more details, see the documentation for [AsyncSnapshotExtension.when].
@@ -38,14 +40,12 @@ extension AsyncListenableBaseExtension<T, Data extends T>
     required R Function(Data data) data,
     required R Function(Object error, StackTrace stackTrace) error,
     required R Function() loading,
-    R Function(Data data)? reloading,
     R Function()? none,
   }) {
     return snapshot.when(
       data: data,
       error: error,
       loading: loading,
-      reloading: reloading,
       none: none,
     );
   }
@@ -56,34 +56,4 @@ extension AsyncListenableNullableExtension<T, Data extends T>
     on AsyncListenableBase<T?, Data?> {
   /// Returns latest data received, failing if there is no data.
   T get requireData => value ?? snapshot.requireData!;
-}
-
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final n = AsyncNotifier<int>(0);
-
-    final value = context.listen(n);
-
-    return const Placeholder();
-  }
-}
-
-extension ContextListenable on BuildContext {
-  T listen<T>(ValueListenable<T> listenable, [ValueChanged<T>? onChanged]) {
-    void _listener() {
-      if (mounted && this is Element) (this as Element).markNeedsBuild();
-      onChanged?.call(listenable.value);
-    }
-
-    listenable.addListener(_listener);
-
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      listenable.removeListener(_listener);
-    });
-
-    return listenable.value;
-  }
 }
